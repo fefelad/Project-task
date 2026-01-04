fb715e2435b1f39943a7decf4ab6a6ce210d7ff0
Sergey Luzin <serzh.mr.s.luzin@mail.ru>
2026-01-04 20:45:19 +0300

    Сделал шрифты, и глобал переменные, и структуру папок

---
+++ TestDip/src/app/App.tsx
@@ -0,0 +1,14 @@
+import styles from  './App.module.css';
+
+function App() {
+  
+  return (
+    <>
+      <div className={styles.container_main}>
+        <p className={styles.header}>Лого</p>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ TestDip/src/shared/ui/Header/Header.tsx
@@ -0,0 +1,15 @@
+
+import styles from './Header.module.css';
+
+function Header() {
+    return(
+        <>
+            <header>
+                <p className={styles.header_logo}>logo</p>
+
+            </header>
+        </>
+    )
+}
+
+export default Header();
