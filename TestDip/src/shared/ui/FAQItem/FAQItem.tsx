fb5776695816f88e40036d73b726eb3cd394b8ce
Сергей <serzh.mr.s.luzin@mail.ru>
2026-04-24 12:33:59 +0300

    Создал новый блок для faq - доделал страницу главную

---
+++ TestDip/src/shared/ui/FAQItem/FAQItem.tsx
@@ -0,0 +1,75 @@
+import { useRef, useState } from 'react';
+import classNames from 'classnames';
+import styles from './FAQItem.module.css';
+import Text from '../Text/Text';
+
+interface FAQItemProps {
+    title: string;
+    text: string;
+}
+
+export const FAQItem = ({ title, text }: FAQItemProps) => {
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef<HTMLDivElement>(null);
+
+    return (
+        <div className={classNames(styles.faqItem, {
+            [styles.open]: isOpen,
+        })}
+        >
+            <button
+                className={styles.header}
+                type="button"
+                onClick={() => setIsOpen((prev) => !prev)}
+            >
+                <Text fontFamily="onest" className={styles.title}>
+                    {title}
+                </Text>
+
+                <span className={styles.icon}>
+                    {isOpen ? (
+                        // минус
+                        <svg
+                            viewBox="0 0 27 2"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1 1L26 1"
+                                stroke="black"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                            />
+                        </svg>
+                    ) : (
+                        // плюс
+                        <svg
+                            viewBox="0 0 27 26"
+                            xmlns="http://www.w3.org/2000/svg"
+                        >
+                            <path
+                                d="M1 13H26M13.5 1V25"
+                                stroke="black"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                            />
+                        </svg>
+                    )}
+                </span>
+            </button>
+
+            <div
+                ref={contentRef}
+                className={styles.content}
+                style={{
+                    maxHeight: isOpen
+                        ? `${contentRef.current?.scrollHeight}px`
+                        : '0px',
+                }}
+            >
+                <Text fontFamily="onest" className={styles.text}>
+                    {text}
+                </Text>
+            </div>
+        </div>
+    );
+};
