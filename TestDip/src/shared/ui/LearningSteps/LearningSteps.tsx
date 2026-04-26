83b2936699c67d3cbb26b31d3ed592988299d173
Сергей <serzh.mr.s.luzin@mail.ru>
2026-04-26 09:54:17 +0300

    Доделал блок с шагами у карточки курса

--- TestDip/src/shared/ui/LearningSteps/LearningSteps.tsx
+++ TestDip/src/shared/ui/LearningSteps/LearningSteps.tsx
@@ -14,18 +14,85 @@ export default function LearningSteps() {
       </Text>
 
       <div className={styles.learnGrid}>
-        {/* карточки */}
-        <div className={`${styles.learnCard} ${styles.step1}`}>Шаг 1</div>
-        <div className={`${styles.learnCard} ${styles.step2}`}>Шаг 2</div>
-        <div className={`${styles.learnCard} ${styles.step3}`}>Шаг 3</div>
-        <div className={`${styles.learnCard} ${styles.step4}`}>Шаг 4</div>
-
-        {/* результат */}
-        <div className={styles.resultCard}>
-          Чему научится ваш ребенок
+        {/* Шаг 1 */}
+        <div className={`${styles.learnCard} ${styles.step1}`}>
+          <div className={styles.cardContent}>
+            <Text className={styles.cardTitle} fontFamily="onest">
+              Шаг 1
+            </Text>
+            <Text className={styles.cardText} fontFamily="onest">
+              Знакомится с основами веб-дизайна и понимает,
+              из каких элементов состоит сайт: блоки, кнопки,
+              меню и изображения.
+            </Text>
+          </div>
         </div>
 
-        {/* стрелки */}
+        {/* Шаг 2 */}
+        <div className={`${styles.learnCard} ${styles.step2}`}>
+          <div className={styles.cardContent}>
+            <Text className={styles.cardTitle} fontFamily="onest">
+              Шаг 2
+            </Text>
+            <Text className={styles.cardText} fontFamily="onest">
+                Осваивает композицию, цвет и шрифты,
+                учится делать дизайн понятным и логичным.
+                Понимает, как визуально управлять вниманием пользователя.
+            </Text>
+          </div>
+        </div>
+
+        {/* Шаг 3 */}
+        <div className={`${styles.learnCard} ${styles.step3}`}>
+          <div className={styles.cardContent}>
+            <Text className={styles.cardTitle} fontFamily="onest">
+              Шаг 3
+            </Text>
+           <Text className={styles.cardText} fontFamily="onest">
+                Создает первые макеты страниц и учится
+                продумывать удобство для пользователя.
+                Понимает, как сделать интерфейс логичным и простым в использовании.
+            </Text> 
+          </div>
+        </div>
+
+        {/* Шаг 4 */}
+        <div className={`${styles.learnCard} ${styles.step4}`}>
+          <div className={styles.cardContent}>
+            <Text className={styles.cardTitle} fontFamily="onest">
+              Шаг 4
+            </Text>
+            <Text className={styles.cardText} fontFamily="onest">
+                Собирает собственный мини-проект:
+                дизайн страницы или сайта и презентует результат.
+                Учится объяснять свои решения и уверенно представлять свою работу.
+            </Text>
+          </div>
+        </div>
+
+        {/* Результат */}
+        <div className={styles.resultCard}>
+          <div className={styles.cardContent}>
+            <Text className={styles.cardTitle} fontFamily="onest">
+              Чему научится ваш ребенок
+            </Text>
+            <Text className={styles.cardText} fontFamily="onest">
+                Ребенок научится не просто рисовать красивые картинки,
+                а мыслить как дизайнер: анализировать задачи, продумывать
+                структуру страницы и создавать удобные интерфейсы.
+                Он сможет самостоятельно разрабатывать визуальные решения,
+                работать с композицией и цветом, а также уверенно
+                объяснять свои идеи и защищать проекты.
+                Освоит базовые инструменты дизайна и научится работать
+                с макетами, понимать принципы пользовательского опыта
+                и создавать проекты, которые выглядят современно и понятно.
+                В результате у ребенка появится не только первый проект
+                в портфолио.
+                </Text>
+          </div>
+        </div>
+
+        {/* Стрелки */}
         <img src={arrow1} className={styles.arrow1} alt="" />
         <img src={arrow2} className={styles.arrow2} alt="" />
         <img src={arrow3} className={styles.arrow3} alt="" />
